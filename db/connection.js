6713485b0a58ef0e3c2347e889627c888e37dc31
OGrunner <orshagoines@gmail.com>
2022-09-05 11:49:56 -0400

    modified schema

---
+++ db/connection.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql2');
+//require('dotenv').config();
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      // MySQL password
+      password: '',
+      database: 'employee_db'
+    }
+  );
+
+module.exports = db;
